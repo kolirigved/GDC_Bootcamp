4da00bd68860d6633e6ab15c820be2c93af3ad55
Rigved Koli <rigvedkoli24@gmail.com>
2023-10-23 00:20:12 +0530

    Update Powerup.cs

--- Assets/Powerup.cs
+++ Assets/Powerup.cs
@@ -6,19 +6,11 @@ public class Powerup : MonoBehaviour
 {
     public GameObject Shield;
     public bool poweractive=false;
-    public float PowerUpDuration = 1.0f;
-    private float ActivationTime;
     void Start(){
         Shield.SetActive(false);
         poweractive=false;
     }
-    void Update(){
-        if (isActivated() && Time.time - ActivationTime >= PowerUpDuration)
-        {
-            DeactivateShield();
-        }
-
-    }
+    
     private void OnTriggerEnter2D(Collider2D collison){
         if(collison.CompareTag("Player")){
             Destroy(gameObject);
@@ -27,9 +19,8 @@ public class Powerup : MonoBehaviour
     }
     public void ActivateShield(){
         Shield.SetActive(true);
-        poweractive=true;
-        ActivationTime = Time.time;
     }
+
     public void DeactivateShield(){
         Shield.SetActive(false);
         poweractive=false;
